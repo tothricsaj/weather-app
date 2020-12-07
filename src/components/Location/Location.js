0399fab400362967aab72c049cfb11f86324d705
Richard Toth <Richard_Toth@epam.com>
2020-12-07 11:53:29 +0100

    Location layout with no style

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Location = props => {
   return (
     <div>
-        Location
+        <button>Search for places</button>
+        <button>Location</button>
+
+        <img src="https://www.metaweather.com//static/img/weather/png/64/lc.png" alt="Weather image"/>
+
+        <p>15 C</p>
+        <p>Shower</p>
+
+        <p>Today - Fri 5 Jun</p>
+        <p>Helsinki</p>
     </div>
   )
 }
