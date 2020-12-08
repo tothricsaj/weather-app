20022c49e52b7c8f8dee1d07e8a77e16cbb869b1
Richard Toth <Richard_Toth@epam.com>
2020-12-08 09:49:32 +0100

    daly temperetaure style

--- src/components/Week/Week.js
+++ src/components/Week/Week.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import style from './weekStyle.module.css'
 
 const DalyWeatherInfo = props => {
   return (
-    <div>
+    <div className={style.dalyWeather}>
       <p>Tomorrow</p>
       <img src="https://www.metaweather.com//static/img/weather/png/64/sn.png" alt=""/>
-      <p>16 C</p>
-      <p>11 C</p>
+      <p className={style.dalyTemperature}>11C</p>
+      <p className={style.dalyTemperature}>16C</p>
     </div>
   )
 }
 
 const Week = props => {
   return (
-    <div>
+    <div className={style.weekWrapper}>
         {
           [1,2,3,4,5].map(dalyWeather => <DalyWeatherInfo />)
         }
