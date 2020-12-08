de75a9e581369ca6186a59a44511df70e8c895b2
Richard Toth <Richard_Toth@epam.com>
2020-12-08 14:44:07 +0100

    basic fetch logic: half-stage

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import style from './Location.module.css'
 
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const baseUrl = 'https://www.metaweather.com/api/location/'
+
 const Location = props => {
+  const [woeId, setWoeId] = useState(null)
+  const [weatherData, setWeatherData] = useState('')
+
+  useEffect(() => {
+    let url
+    navigator.geolocation.getCurrentPosition(pos => {
+      console.log(pos.coords.latitude, pos.coords.longitude)
+
+      url = `${baseUrl}search/?lattlong=${pos.coords.latitude},${pos.coords.longitude}`
+
+      fetch(proxyUrl + url)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data)
+        setWoeId(data[0].woeid)
+      }).catch(err => console.log(err))
+    })
+  }, [])
+
+  useEffect(() => {
+    if(woeId) {
+      console.log('Fetch the weather datas')
+      fetch(proxyUrl + baseUrl + woeId)
+        .then(res => res.json())
+        .then(data => {
+          // console.log(data)
+          const today = data.consolidated_weather[0]
+          const weatherInfos = {
+            imgAbbr: today.weather_state_abbr,
+            state: today.weather_state_name,
+            date: today.applicable_date,
+            temperature: today.the_temp,
+            city: data.title,
+          }
+
+          console.log(weatherInfos)
+          setWeatherData(weatherInfos)
+        }).catch(err => console.log(err))
+      }
+  }, [woeId])
+
   return (
     <div className={style.location}>
       <div className={style.buttons}>
@@ -10,17 +54,19 @@ const Location = props => {
       </div>
 
       <div className={style.weahterImg}>
-        <img src="https://www.metaweather.com/static/img/weather/png/64/lc.png" alt="Weathe"/>
+        <img
+          src={`https://www.metaweather.com/static/img/weather/png/64/${weatherData ? weatherData.imgAbbr: 'lc'}.png`}
+          alt="Weathe"/>
       </div>
 
       <div className={style.temerature}>
-        <p className={style.tempValue}>15 C</p>
+        <p className={style.tempValue}>{weatherData ? weatherData.temerature : null} C</p>
         <h1>Shower</h1>
       </div>
 
       <div className={style.date}>
-        <p>Today - Fri 5 Jun</p>
-        <p>Helsinki</p>
+        <p>Today - {weatherData ? weatherData.date : null}</p>
+        <p>{weatherData.city}</p>
       </div>
     </div>
   )
