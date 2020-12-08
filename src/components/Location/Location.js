0afa0f474d6a0023005ae99a184cd1638cad2047
Richard Toth <Richard_Toth@epam.com>
2020-12-08 10:46:20 +0100

    location style

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import style from './Location.module.css'
 
 const Location = props => {
   return (
-    <div>
-        <button>Search for places</button>
-        <button>Location</button>
+    <div className={style.location}>
+      <div className={style.buttons}>
+        <button className={style.search}>Search for places</button>
+        <button className={style.search}>Location</button>
+      </div>
 
+      <div className={style.weahterImg}>
         <img src="https://www.metaweather.com/static/img/weather/png/64/lc.png" alt="Weathe"/>
+      </div>
 
-        <p>15 C</p>
-        <p>Shower</p>
+      <div className={style.temerature}>
+        <p className={style.tempValue}>15 C</p>
+        <h1>Shower</h1>
+      </div>
 
+      <div className={style.date}>
         <p>Today - Fri 5 Jun</p>
         <p>Helsinki</p>
+      </div>
     </div>
   )
 }
